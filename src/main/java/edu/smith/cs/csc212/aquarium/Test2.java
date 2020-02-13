b2ce5efae07ad581d1487157a4cd12b4f017cbdb
Winnie Zong <wzong@smith.edu>
2020-02-12 20:15:31 -0500

    hh

--- src/main/java/edu/smith/cs/csc212/aquarium/Test2.java
+++
@@ -1,9 +0,0 @@
-package edu.smith.cs.csc212.aquarium;
-
-public class Test2 {
-	public static void main(String[] args){
-		int result=(int)(Math.random()*500);
-		System.out.println(result);
-	}
-
-}
